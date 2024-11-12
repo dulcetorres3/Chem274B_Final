1022d840bd35ea98b929dcbef1fddc1fb9e8f9e9
daredrouin <dariendrouin@gmail.com>
2024-11-12 08:50:52 -0500

    Update banking_system_impl.py
    Completed constructor for Level 1 and implemented create_account method.

--- banking_system_impl.py
+++ banking_system_impl.py
@@ -4,7 +4,18 @@ from banking_system import BankingSystem
 class BankingSystemImpl(BankingSystem):
 
     def __init__(self):
-        # TODO: implement
-        pass
+        # Dictionary containing account_id (key) and amount (value)
+        self.accounts = {}
 
     # TODO: implement interface methods here
+    def create_account(self, timestamp: int, account_id: str) -> bool:
+        
+        # Return false if account already exists
+        if account_id in self.accounts:
+            return False
+            
+        # Initialize new account with balance of zero and return True
+        self.accounts[account_id] = 0
+        return True
+        
+    
