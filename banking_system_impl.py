51377bbbc4eab7a602022970a87154e764a0b75c
Dare Drouin <dared@berkeley.edu>
2024-12-06 20:33:09 -0500

    Succesfully added merge_accounts, still working through get_balance.

--- banking_system_impl.py
+++ banking_system_impl.py
@@ -1,5 +1,5 @@
 from banking_system import BankingSystem
-import time
+
 
 class BankingSystemImpl(BankingSystem):
 
@@ -10,6 +10,7 @@ class BankingSystemImpl(BankingSystem):
         self.payment_counter = 0 # Incrementer used to track unique payments
         self.pending_cashbacks = [] # List of list to track cashback information (timestamp, account_id, payment_id, amount)
         self.payments = {} # Tracks account_ID and it's corresponding payment_IDs.
+        self.balance_history = {}
 
     # TODO: implement interface methods here
     
@@ -18,8 +19,12 @@ class BankingSystemImpl(BankingSystem):
         if account_id in self.accounts:
             return False
             
+        
         # Initialize new account with balance of zero and return True
         self.accounts[account_id] = 0
+        # Track timestamp when account was created 
+        self.balance_history[account_id] = []
+        self.balance_history[account_id].append((timestamp, 0))
         self.outgoing_transactions[account_id] = 0
         return True
         
@@ -34,6 +39,9 @@ class BankingSystemImpl(BankingSystem):
 
         # Add given amount to provided account
         self.accounts[account_id] += amount
+
+        #update balance history
+        self.balance_history[account_id].append((timestamp, self.accounts[account_id]))
       
         # Return the new balance of the account
         return self.accounts[account_id]
@@ -61,6 +69,10 @@ class BankingSystemImpl(BankingSystem):
 
         # Update outgoing transactions with transfered amount
         self.outgoing_transactions[source_account_id] += amount
+
+        #update balance history
+        self.balance_history[source_account_id].append((timestamp, self.accounts[source_account_id]))
+        self.balance_history[target_account_id].append((timestamp, self.accounts[target_account_id]))
 
         # return balance of source
         return self.accounts[source_account_id]
@@ -115,6 +127,9 @@ class BankingSystemImpl(BankingSystem):
             self.payments[account_id] = []
         self.payments[account_id].append(payment_id)
 
+        #update balance history
+        self.balance_history[account_id].append((timestamp, self.accounts[account_id]))
+
         # Return the payment_ID of the transaction
         return payment_id
 
@@ -131,6 +146,16 @@ class BankingSystemImpl(BankingSystem):
 
                 # Remove the pending cashback transaction after it has been processed
                 self.pending_cashbacks.remove((cashback_time, account_id, payment_id, cashback))
+            #update balance history
+            self.balance_history[account_id].append((timestamp, self.accounts[account_id]))
+
+    """
+    HELPER FUNCTION FOR BALANCE HISTORY
+    def update_balance_history(self, account_id: str, timestamp: int):
+        if account_id not in self.balance_history:
+            self.balance_history[account_id] = []
+        self.balance_history[account_id].append((timestamp, self.accounts[account_id]))
+    """
 
     def get_payment_status(self, timestamp: int, account_id: str, payment: str) -> str | None:
 
@@ -159,7 +184,79 @@ class BankingSystemImpl(BankingSystem):
                 
         # If this point is reached, cashback has been successfully processed.
         return "CASHBACK_RECEIVED"
+    
+    def merge_accounts(self, timestamp: int, account_id_1: str, account_id_2: str) -> bool:
+        
+        #check if account 1 is equal to account 2
+        if account_id_1 == account_id_2:
+            return False
+        # check if account 1 exist
+        if account_id_1 not in self.accounts:
+            return False
+        # check if account 2 exist
+        if account_id_2 not in self.accounts: 
+            return False
 
+        # Cashback for account 2 refunded to account 1
+        for i, (cashback_time, account_id, payment_id, cashback) in enumerate(self.pending_cashbacks):
+            if account_id == account_id_2:
+                self.pending_cashbacks[i] = (cashback_time, account_id_1, payment_id, cashback)
+        
+        # Update payment transactions
+        self.payments[account_id_1] = self.payments.get(account_id_1, []) + self.payments.pop(account_id_2, [])
+        
+        # Add balance of account 2 to account 1
+        self.accounts[account_id_1]+= self.accounts[account_id_2]
+        
+        #top spenders (account 2 in outgoing transactions)
+        if account_id_2 in self.outgoing_transactions: 
+            #add account 2 outgoing transactions to account 1
+            self.outgoing_transactions[account_id_1] += self.outgoing_transactions[account_id_2]
+            
+        # delete merged account from account dictionary
+        del self.accounts[account_id_2]
+
+        # delete merged account from outgoing transactions
+        del self.outgoing_transactions[account_id_2]
+
+        #update balance history
+        self.balance_history[account_id_1].append((timestamp, self.accounts[account_id_1]))
+
+        # Balance history for account 2 goes into account 1
+        self.balance_history[account_id_1].extend(self.balance_history[account_id_2])
+        
+        # Delete balance history for account 2
+        del self.balance_history[account_id_2]
+        
+        # Successful merge
+        return True
+    
+    def get_balance(self, timestamp: int, account_id: str, time_at: int) -> int | None:
+        # Process pending cashbacks, if applicable
+        self.process_cashback(timestamp)
+    
+        if account_id not in self.balance_history:
+            return None
+        """""""""
+        # account not exist at given time
+        if self.balance_history[account_id][0][0] < time_at:
+            return None
+        """
+
+        # get balance at time_at
+        for time, balance in self.balance_history[account_id]:
+            if time == time_at:
+                return balance
+        
+        
 if __name__ == "__main__":
     
     bs = BankingSystemImpl()
+
+    print(bs.create_account(1, "account1"))
+    print(bs.create_account(2, "account2"))
+    print(bs.deposit(3, "account1", 500))
+    print(bs.deposit(4, "account2", 600))
+    print(bs.merge_accounts(5, "account1", "account2"))
+    print(bs.get_balance(6, "account1", 3))
+    print(bs.get_balance(7, "account1", 5))
